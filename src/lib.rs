d7fd93de942bbd211410b45641d1574fb6dde260
Ryn Langer <rlanger579@gmail.com>
2024-08-17 21:25:58 -0700

    Go through errors and suggestions from rust analyzer

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,21 @@ use std::*;
 
 use io::Error;
 
-
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
-pub enum FileType{
+#[derive(Debug, Clone)]
+pub enum FileType {
     Delimited { delimiter: char },
-    FixWidth { positions: Vec<u16>}
+    FixWidth { positions: Vec<u16> },
 }
 
 #[derive(Debug, Copy, Clone)]
-pub struct CellContent{
-    
-}
+pub struct CellContent {}
 
 #[derive(Debug, Clone)]
-pub struct LineContent{
-    pub cells : Vec<CellContent>
-    
+pub struct LineContent {
+    pub cells: Vec<CellContent>,
 }
 /*
 ToDo:
@@ -28,117 +25,114 @@ ToDo:
  * Split based on line ending
  * For each line:
      *  create a cell entry for each column, based on headers
-     * Support modifying a cell's content, and modifying some tracker on the FileInfo? 
+     * Support modifying a cell's content, and modifying some tracker on the FileInfo?
          Could potentially just flag the FileInfo as modified as an indication that we have anything to write
   */
 #[derive(Debug, Clone)]
 // #[derive(Debug)]
-pub struct FileInfo<'a>{
+pub struct FileInfo<'a> {
     pub path: &'a str,
     pub file_type: FileType,
     pub has_header: bool,
-    pub header: Option<LineContent>
+    pub header: Option<LineContent>,
 }
-impl FileInfo{
-    fn load(&mut self) -> Vec<LineContent>{
+impl<'a> FileInfo<'a> {
+    fn load(&mut self) -> Vec<LineContent> {
         let mut line_count = 0;
         let mut header_line = None::<LineContent>;
         let mut output = Vec::<LineContent>::new();
-        let content = fs::read_to_string(* self.path);
-        if let Error = content{
-            panic!("Unable to read file at {}", * self.path);
-        }
-        for text in content.lines(){
-            let mut cell_split = Vec::<&str>::new();
-            match self.FileType{
-                 FileType::Delimited(delim) => cellSplit = SplitDelimited(delim, text),
-                 FileType::FixWidth(positions) => cellSplit = 
+        let file_result = fs::read_to_string(&self.path);
+        let content = match file_result {
+            Ok(content) => content,
+            Err(e) => panic!("Unable to read file at {}", &self.path),
+        };
+        // ToDo: get content as string instead of Result
+        for text in content.lines() {
+            let mut cell_split = match self.file_type {
+                FileType::Delimited { delimiter } => split_delimited(delimiter, text),
+                FileType::FixWidth { positions } => split_by_positions(positions, text),
             };
-            let line = LineContent::new(cellSplit);
+            let line = LineContent::new(cell_split);
             if line_count == 0 {
-                if self.HasHeader{
-                    self.Header = Some(line);
-                }
-                else{
-                    self.Header = None;
+                if self.has_header {
+                    self.header = Some(line);
+                } else {
+                    self.header = None;
                     output.push(line);
-                }    
-            }
-            else{
+                }
+            } else {
                 output.push(line);
             }
-            lineCount = lineCount + 1;
-            
-        };
+            line_count = line_count + 1;
+        }
         output
     }
 }
-fn SplitDelimited(delim: char, src: &str) -> Vec<Option<&str>>{
-    
-}
+fn split_delimited(delim: char, src: &str) -> Vec<Option<&str>> {}
 //Might need to change this to Vec<Option<string>> ? Need to research a bit more and practice with borrow/ownership of string content
-fn SplitByPositions(positions: vec<u16>, src: &str) -> Vec<Option<&str>>{
+fn split_by_positions(positions: Vec<u16>, src: &str) -> Vec<Option<&str>> {
     let mut lp = 0u16;
-    let mut colCount = positions.len();
+    let mut col_count = positions.len();
     // if positions[colCount] == src.len(){
     //    colCount = colCount - 1;
     // }
-    assert_ne!(0, colCount);
-    let mut colIndices = src.char_indices();
-    let mut start = positions[0];// 0usize;
-    let mut strEnd;
-    let vs = Vec::<&str>::with_capacity(colCount);
+    assert_ne!(0, col_count);
+    let mut col_indices = src.char_indices();
+    let mut start = positions[0]; // 0usize;
+    let mut str_end: usize = 0;
+    let mut vs = Vec::<Option<&str>>::with_capacity(col_count);
     // Since positions is starting positions, our last column will need to be populated after this loop.
-    for p in positions.iter().enumerate(){
-        if p==lp && p == start {
-            if start > 0{
-               match colIndices.nth(start){
-                  Some(s, _) => start = s,
-                  None =>,
-               };
+    for pos_iter in positions.iter().enumerate() {
+        let (pos, _) = pos_iter;
+        let p = pos as u16;
+        if p == lp && p == start {
+            if start > 0 {
+                if let Some(s) = col_indices.nth(start as usize) {
+                    start = s.0 as u16;
+                }
             }
-             
-            continue;// Columns positions are start of column, rather than end of column.
+            continue; // Columns positions are start of column, rather than end of column.
         }
-        let endingIndex = p - lp;
+        let ending_index = u16::from(p) - lp;
 
-        match colIndices.nth(endingIndex){
-            Some(colEnd,_) => {
+        match col_indices.nth(usize::from(ending_index)) {
+            Some(colEnd) => {
                 let st = usize::from(start);
-                let s = &src[st..colEnd];
+                let (end, _) = colEnd;
+                let s = &src[st..end];
                 vs.push(Some(&s));
-                start = colEnd; // Note: should be exclusive ending position, so should not need to modify before assigning to start
-            },
-            None =>  vs.push(None),
+                start = end as u16; // Note: should be exclusive ending position, so should not need to modify before assigning to start
+            }
+            None => vs.push(None),
         };
         /*example: Field1   Field2 Field3  (EOL)
-        Start at 0, then we want to look for the position of F in Field2 (10,  field 1 is len 9)
-        File1: 0..10
-        For field2, we would want 10..18 (exclusive on right side)
-        For Field3, we would want 18..26 (Ignore EOL)
-        What if we want to throw out some beginning space consistently? Have first column start later
-        E.g. 
-    Field1  Field2  Field3  '
-        4 = Start, so:
-        4..12
-        12..20
-        20..
-        
-        */
+            Start at 0, then we want to look for the position of F in Field2 (10,  field 1 is len 9)
+            File1: 0..10
+            For field2, we would want 10..18 (exclusive on right side)
+            For Field3, we would want 18..26 (Ignore EOL)
+            What if we want to throw out some beginning space consistently? Have first column start later
+            E.g.
+        Field1  Field2  Field3  '
+            4 = Start, so:
+            4..12
+            12..20
+            20..
+
+            */
         lp = p;
     }
     //Get content of last column in line, or None if we finished the string while going through previous columns
-    loop{
-        match colIndices.next(){
-            Some(colEnd, _) => strEnd = colEnd,
-            None=> break,
+    loop {
+        match col_indices.next() {
+            Some(colEnd) => str_end = colEnd.0,
+            None => break,
         };
-    };
-    if strEnd > start{
-        let last =  &src[start..strEnd];
-        vs.push(Some(&last));   
-    }   
-    else{
+    }
+
+    if str_end > start as usize {
+        let last = &src[start as usize..str_end];
+        vs.push(Some(&last));
+    } else {
         vs.push(None);
     }
     vs
@@ -152,9 +146,9 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
-    fn test_column_parse_positions(){
-        let cols = [4, 9, 14, 21];
-        let input = "Col1Start Col2Next to last column"
-        let vs = SplitByPositions(positions, input );
+    fn test_column_parse_positions() {
+        let cols = vec![4u16, 9u16, 14u16, 21u16];
+        let input = "Col1Start Col2Next to last column";
+        let vs = split_by_positions(cols, input);
     }
 }
